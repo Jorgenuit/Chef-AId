6d5da9da7aa30737213143e18322c8b5db572657
Jorgenuit <jhe137@uit.no>
2025-05-26 16:24:29 +0200

    front-end progress

---
+++ src/chef-ai/app/components/Bubble.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import "./bubble.css";
+
+interface MessageBubbleProps {
+  text: string;
+  sender: "user" | "reply";
+}
+
+const MessageBubble: React.FC<MessageBubbleProps> = ({ text, sender }) => {
+  return <div className={'message-bubble ${sender}'}>{text}</div>;
+};
+
+export default MessageBubble;
